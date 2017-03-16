b5eaac9aed96fc099deba739ba9c7c91d032485a
jiaqifeng <jiaqifeng@gmail.com>
2017-03-16 15:18:33 +0800

    change activemq comsumer to access echo service to extend the topology.

--- activemqconsumer/src/main/java/com/jack/pinpoint/activemq/consumer/Receiver.java
+++ activemqconsumer/src/main/java/com/jack/pinpoint/activemq/consumer/Receiver.java
@@ -2,7 +2,13 @@ package com.jack.pinpoint.activemq.consumer;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
- 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
@@ -39,6 +45,7 @@ public class Receiver
 		    TextMessage message = (TextMessage) consumer.receive(100000);
 		    if (null != message) {
 			System.out.println("recevie message:" + message.getText());
+			onMessage(message.getText());
 		    } else {
 			break;
 		    }
@@ -52,6 +59,61 @@ public class Receiver
                     connection.close();
             } catch (Throwable ignore) {
             }
+        }
+    }
+
+    public static void onMessage(String data) {
+	try { throw new IOException("jack test pinpont"); } catch (IOException e) { e.printStackTrace();};
+        System.out.println("\n-------------------- received: " + data+"\n");
+
+        try {
+            InputStream inputStream = null;
+            InputStreamReader inputStreamReader = null;
+            BufferedReader reader = null;
+            StringBuffer resultBuffer = new StringBuffer();
+            String tempLine = null;
+
+            URL url = new URL("http://localhost:8099/echo-websvr/hello");//http://www.baidu.com");
+            HttpURLConnection conn = (HttpURLConnection )url.openConnection();
+
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setUseCaches(false);
+            conn.setConnectTimeout(2*1000);
+            conn.setReadTimeout(2*1000);
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            if (conn.getResponseCode() >= 300) {
+                throw new Exception(
+                        "HTTP Request is not success, Response code is " + conn.getResponseCode());
+            }
+
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                inputStream = conn.getInputStream();
+                inputStreamReader = new InputStreamReader(inputStream);
+                reader = new BufferedReader(inputStreamReader);
+
+                while ((tempLine = reader.readLine()) != null) {
+                    resultBuffer.append(tempLine);
+                    resultBuffer.append("\n");
+                }
+            }
+
+//            OutputStream outs = conn.getOutputStream();
+//            InputStream ins = conn.getInputStream();
+//
+//            int count = 10;
+//            byte[] buf = new byte[count];
+//            int readCount = 0;
+//            while (readCount < count) {
+//                readCount += ins.read(buf, readCount, count - readCount);
+//            }
+//            String msg = new String(buf);
+            System.out.println("MyMessageListener get echo response" + resultBuffer.toString());
+        } catch (Exception e) {
+            System.out.println("MyMessageListener got exception:"+e);
+            e.printStackTrace();
         }
     }
 }
