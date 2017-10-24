a81d610f4e5806f760047ad0f868985277d46541
jiaqifeng <jiaqifeng@gmail.com>
2017-10-24 14:30:11 +0800

    dubbo-echo-client: add log4j for testing.

--- dubbo-echo-client/src/main/java/com/jack/pinpoint/dubbo/client/DubboClientServlet.java
+++ dubbo-echo-client/src/main/java/com/jack/pinpoint/dubbo/client/DubboClientServlet.java
@@ -7,15 +7,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import org.apache.log4j.Logger;
+
 /**
  * Created by jack on 17-4-18.
  */
 public class DubboClientServlet extends HttpServlet {
 
+    static Logger logger = Logger.getLogger(DubboClientServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter out=response.getWriter();
+
+        logger.info("DubboClientServlet.doGet(): --------++++++++++++++++++---------------_++++++++++++++");
 
         EchoClient client = new EchoClient();
         String echo=client.echo("lisa");
