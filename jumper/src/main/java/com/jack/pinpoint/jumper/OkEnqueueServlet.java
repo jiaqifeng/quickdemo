6db2aa89fb348314d2723f01bfa1e47c8e12e965
jiaqifeng <jiaqifeng@gmail.com>
2017-10-26 10:03:52 +0800

    jumper: using okHttpClient.queue, some classical multi-thread bug happens, fix them at last.

--- jumper/src/main/java/com/jack/pinpoint/jumper/OkEnqueueServlet.java
+++ jumper/src/main/java/com/jack/pinpoint/jumper/OkEnqueueServlet.java
@@ -4,6 +4,8 @@ import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,11 +14,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.rmi.server.ExportException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by jack on 17-10-25.
  */
 public class OkEnqueueServlet extends HttpServlet {
+    static Logger logger = LoggerFactory.getLogger(HttpClientServlet.class);
+    //volatile String result;
+    String result;
 
     @Override
     protected void doGet(HttpServletRequest req, final HttpServletResponse response)
@@ -25,20 +32,43 @@ public class OkEnqueueServlet extends HttpServlet {
 
         Request request = new Request.Builder().url(url).build();
         OkHttpClient client = new OkHttpClient();
+        logger.info(" -------------------- call echo using OkHttpClient.enqueue --------------------");
+
+        // we have to use a latch, or the response may be closed before the Callback
+        final CountDownLatch latch = new CountDownLatch(1);
+        result = "undefined";
         client.newCall(request).enqueue(new Callback() {
-            
+
             public void onFailure(Request request, IOException e) {
-                try {
-                    PrintWriter out = response.getWriter();
-                    out.println("get echo failed");
-                } catch (Exception ee) {}
+                /* I got interesting result here, when latch.countDown() called at next line instead
+                 * after set result, the out.println may sometime return undefined. At least it is
+                 * really ofter for one day, about 1/3 times it will apear.
+                 * Even I add the volatile for result does not work, so I think it means after
+                 * latch.countDown(), the tomcat io thread get to execute real quickly sometimes.
+                 *
+                 * With or without pinpoint agent, it happens too.
+                 *
+                 * If you use response here, it must be final, for response is a local variable in java,
+                 * not like nodejs which is auto treated as closure, so I make result a outer class field.
+                 */
+                //latch.countDown();
+                logger.info(" -------------------- call echo using OkHttpClient.enqueue onFailure --------------------");
+                result = "failed";
+                latch.countDown();
             }
 
             public void onResponse(Response resp) throws IOException {
-                PrintWriter out=response.getWriter();
-                out.println("get "+resp.body().string());
+                //latch.countDown();
+                logger.info(" -------------------- call echo using OkHttpClient.enqueue onResponse --------------------");
+                result = resp.body().string();
+                latch.countDown();
             }
         });
+        try {
+            latch.await(5, TimeUnit.SECONDS);
+        } catch (Exception e) {}
+        PrintWriter out = response.getWriter();
+        out.println("get echo "+result);
     }
 }
 
