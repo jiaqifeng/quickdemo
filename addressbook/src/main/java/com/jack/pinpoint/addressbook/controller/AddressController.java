37c48008f29f3f9d4d2051bf86ab93dc4678b310
jiaqifeng <jiaqifeng@gmail.com>
2017-12-11 15:45:22 +0800

    addressbook: upate jdbcTemplate to test sql attack.

--- addressbook/src/main/java/com/jack/pinpoint/addressbook/controller/AddressController.java
+++ addressbook/src/main/java/com/jack/pinpoint/addressbook/controller/AddressController.java
@@ -7,8 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -26,6 +31,12 @@ public class AddressController {
     @Autowired
     private ApplicationContext applicationContext;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    // test PreparedStatementExecuteQueryInterceptor
+    // & PreparedStatementBindVariableInterceptor
+    // & PreparedStatementCreateInterceptor
     @RequestMapping(value = "/api/v1/address", method = RequestMethod.GET)
     @ResponseBody
     public Result<Address> findOneAddress(Integer id, String name) {
@@ -56,6 +67,80 @@ public class AddressController {
             Integer count=addressDao.insertAddress(address);
             if (count==1) {
                 result.success(address.getId());
+                return result;
+            }
+        } catch (Exception e){
+            logger.error("can't insert general address", e);
+            result.fail("500", "服务器错误");
+        }
+
+        return result;
+    }
+
+    // test StatementExecuteQueryInterceptor
+    @RequestMapping(value = "/api/v1/address/query", method = RequestMethod.GET)
+    @ResponseBody
+    public Result<Address> queryAddress(String name) {
+        Result<Address> result = Result.create();
+        try {
+            String sql="select id, name, address, mobile from addresses where name='"+name+"'";
+            logger.info("sql = "+sql);
+            //Address src=jdbcTemplate.queryForObject(sql, Address.class);
+            List<Address> rs = jdbcTemplate.query(sql, new RowMapper<Address>() {
+                public Address mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    Address r = new Address();
+                    r.setId(rs.getInt("id"));
+                    r.setName(rs.getString("name"));
+                    r.setAddress(rs.getString("address"));
+                    r.setMobile(rs.getString("mobile"));
+                    return r;
+                }
+            });
+            if (rs.size() < 1) {
+                result.fail("500", "error occured");
+            } else {
+                result.success(rs.get(0));
+            }
+            return result;
+        } catch (Exception e){
+            logger.error("can't insert general address", e);
+            result.fail("500", "服务器错误");
+        }
+
+        return result;
+    }
+
+    // test StatementExecuteUpdateInterceptor
+    @RequestMapping(value = "/api/v1/address/add", method = RequestMethod.GET)
+    @ResponseBody
+    public Result<Integer> addAddress(String name, String address) {
+        Result<Integer> result = Result.create();
+        try {
+            String sql="insert into addresses(name, address, mobile) VALUES ('"+name+"', '"+address+"','')";
+            logger.info("sql = "+sql);
+            Integer count=jdbcTemplate.update(sql);
+            if (count==1) {
+                result.success(count);
+                return result;
+            }
+        } catch (Exception e){
+            logger.error("can't insert general address", e);
+            result.fail("500", "服务器错误");
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/api/v1/address/del", method = RequestMethod.GET)
+    @ResponseBody
+    public Result<Integer> delAddress(String name) {
+        Result<Integer> result = Result.create();
+        try {
+            String sql="DELETE FROM addresses WHERE name='"+name+"'";
+            logger.info("sql = "+sql);
+            Integer count=jdbcTemplate.update(sql);
+            if (count==1) {
+                result.success();
                 return result;
             }
         } catch (Exception e){
